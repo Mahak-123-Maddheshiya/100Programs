cf6a2ad72bbd41f1ceca00ddb8dc539449d6a234
Mahak-123-Maddheshiya <mahakmaddheshiya81@gmail.com>
2026-09-14 21:43:39 +0530

    Initial commit with 100 programs

--- LeapYear.java
+++ LeapYear.java
@@ -1,7 +1,6 @@
 import java.util.Scanner;
 public class LeapYear{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args){        Scanner sc = new Scanner(System.in);
         System.out.print("Enter the year");
         int year = sc.nextInt();
       if(year %400==0){
